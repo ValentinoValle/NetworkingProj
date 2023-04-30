b4d8a56404d21cb493ab358b11b77169ae7f04c7
Valentino Valle <valentino123.valle@gmail.com>
2023-04-30 14:13:44 -0300

    added crawlPage function and main function in main

---
+++ main.js
@@ -0,0 +1,17 @@
+const { crawlPage } = require('./tests_js/crawl.js');
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log('No website provided');
+        process.exit(1);
+    } else if (process.argv.length > 3) {
+        console.log('too many websites provided');
+        process.exit(1);
+    }  
+    const baseURL = process.argv[2];
+  
+    console.log(`starting crawl of ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main()
